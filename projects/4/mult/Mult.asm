79e2db6b635bd519453bbbd59a7df3321bf7c4b8
Alex Pricinoc <alex.pricinoc@icloud.com>
2024-04-26 14:34:46 +0100

    implement Mult.asm

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+    // R2 = 0
+    @R2
+    M=0
+    // i = RAM[R0]
+    @R0
+    D=M
+    @i
+    M=D
+(LOOP)
+    // if (i == 0) goto END
+    @i
+    D=M
+    @END
+    D;JEQ
+    // R2 = R2 + R1
+    @R2
+    D=M
+    @R1
+    D=D+M
+    @R2
+    M=D
+    // i = i - 1
+    @i
+    M=M-1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
