379158e4317e0454b95b48ff02381fbfd1279003
Alex Pricinoc <alex.pricinoc@icloud.com>
2024-05-17 14:26:59 +0100

    passing NestedCall test

--- vm-to-asm/src/code_writer.rs
+++ vm-to-asm/src/code_writer.rs
@@ -8,6 +8,7 @@ pub struct CodeWriter {
     output_file: Option<File>,
     module: Option<String>,
     jump_counter: u16,
+    return_counter: u16,
 }
 
 impl CodeWriter {
@@ -19,6 +20,7 @@ impl CodeWriter {
         let file = File::create(name).expect("set_filename: file not found");
         self.output_file = Some(file);
         self.jump_counter = 0;
+        self.return_counter = 0;
     }
 
     pub fn set_module_name(&mut self, name: &str) {
@@ -44,12 +46,11 @@ impl CodeWriter {
 
                 writeln!(
                     out,
-                    "\
-@SP
-AM=M-1
-D=M
-A=A-1
-M=M{op}D // {command}"
+                    "    @SP
+    AM=M-1
+    D=M
+    A=A-1
+    M=M{op}D // {command}"
                 )
             }
 
@@ -62,10 +63,9 @@ M=M{op}D // {command}"
 
                 writeln!(
                     out,
-                    "\
-@SP
-A=M-1
-M={op}M // {command}"
+                    "    @SP
+    A=M-1
+    M={op}M // {command}"
                 )
             }
 
@@ -83,23 +83,22 @@ M={op}M // {command}"
 
                 writeln!(
                     out,
-                    "\
-@SP
-AM=M-1
-D=M
-A=A-1
-D=M-D
-@__{branch}_{id}_true
-D;{jump}
-D=0
-@__{branch}_{id}_end
-0;JMP
-(__{branch}_{id}_true)
-D=-1
-(__{branch}_{id}_end)
-@SP
-A=M-1
-M=D // {command}"
+                    "    @SP
+    AM=M-1
+    D=M
+    A=A-1
+    D=M-D
+    @__{branch}_{id}_true
+    D;{jump}
+    D=0
+    @__{branch}_{id}_end
+    0;JMP
+    (__{branch}_{id}_true)
+    D=-1
+    (__{branch}_{id}_end)
+    @SP
+    A=M-1
+    M=D // {command}"
                 )
             }
 
@@ -123,10 +122,9 @@ M=D // {command}"
                 "constant" => {
                     writeln!(
                         out,
-                        "\
-@{index}
-D=A
-{PUSH_REGD} // push {segment} {index}"
+                        "    @{index}
+    D=A
+    {PUSH_REGD}       // push {segment} {index}"
                     )
                 }
                 "argument" | "local" | "this" | "that" => {
@@ -134,13 +132,12 @@ D=A
 
                     writeln!(
                         out,
-                        "\
-@{symbol}
-D=M
-@{index}
-A=A+D
-D=M
-{PUSH_REGD} // push {segment} {index}"
+                        "    @{symbol}
+    D=M
+    @{index}
+    A=A+D
+    D=M
+    {PUSH_REGD}             // push {segment} {index}"
                     )
                 }
                 "pointer" | "temp" => {
@@ -153,21 +150,17 @@ D=M
 
                     writeln!(
                         out,
-                        "\
-@{symbol}
-D=M
-{PUSH_REGD}
-"
+                        "    @{symbol}
+    D=M
+    {PUSH_REGD}"
                     )
                 }
                 "static" => {
                     writeln!(
                         out,
-                        "\
-@{static_symbol}
-D=M
-{PUSH_REGD}
-"
+                        "    @{static_symbol}
+    D=M
+    {PUSH_REGD}"
                     )
                 }
 
@@ -179,19 +172,18 @@ D=M
 
                     writeln!(
                         out,
-                        "\
-@{symbol}
-D=M
-@{index}
-D=D+A
-@R13
-M=D
-@SP
-AM=M-1
-D=M
-@R13
-A=M
-M=D"
+                        "    @{symbol}
+    D=M
+    @{index}
+    D=D+A
+    @R13
+    M=D
+    @SP
+    AM=M-1
+    D=M
+    @R13
+    A=M
+    M=D"
                     )
                 }
                 "pointer" | "temp" => {
@@ -205,24 +197,22 @@ M=D"
 
                     writeln!(
                         out,
-                        "\
-@SP
-AM=M-1
-D=M
-@{symbol}
-M=D"
+                        "    @SP
+    AM=M-1
+    D=M
+    @{symbol}
+    M=D // pop {symbol} {index}"
                     )
                 }
 
                 "static" => {
                     writeln!(
                         out,
-                        "\
-@SP
-AM=M-1
-D=M
-@{static_symbol}
-M=D",
+                        "    @SP
+    AM=M-1
+    D=M
+    @{static_symbol}
+    M=D"
                     )
                 }
 
@@ -236,11 +226,7 @@ M=D",
     pub fn write_label(&mut self, label: &str) -> Result<()> {
         let out = self.output_file.as_mut().expect("file is set");
 
-        writeln!(
-            out,
-            "\
-({label})"
-        )
+        writeln!(out, "({label})")
     }
 
     pub fn write_if(&mut self, label: &str) -> Result<()> {
@@ -248,45 +234,80 @@ M=D",
 
         writeln!(
             out,
-            "\
-@SP
-AM=M-1
-D=M
-@{label}
-D;JGT"
+            "    @SP
+    AM=M-1
+    D=M
+    @{label}
+    D;JGT"
         )
     }
 
     pub fn write_goto(&mut self, label: &str) -> Result<()> {
         let out = self.output_file.as_mut().expect("file is set");
 
-        writeln!(out, "@{label}\n0;JMP")
+        writeln!(out, "    @{label}\n    0;JMP")
     }
 
     pub fn write_function(&mut self, name: &str, n_vars: u16) -> Result<()> {
         let out = self.output_file.as_mut().expect("file is set");
 
-        writeln!(out, "({name})")?;
+        writeln!(out, "({name})      // function {name} {n_vars}")?;
 
-        for i in 0..n_vars {
-            writeln!(
-                out,
-                "\
-@LCL
-D=M
-@{i}
-A=A+D
-D=M
-@SP
-M=M+1 // push {i}"
-            )?;
+        if n_vars > 0 {
+            writeln!(out, "    D=0")?;
+
+            for _ in 0..n_vars {
+                writeln!(out, "    {PUSH_REGD}       // push 0")?;
+            }
         }
 
         Ok(())
     }
 
     pub fn write_call(&mut self, name: &str, n_args: u16) -> Result<()> {
-        unimplemented!()
+        let out = self.output_file.as_mut().expect("file is set");
+
+        self.return_counter += 1;
+
+        let ret_addr_label = format!("{name}$ret.{counter}", counter = self.return_counter);
+
+        writeln!(
+            out,
+            "    @{ret_addr_label} // call {name} {n_args}
+    D=A
+    {PUSH_REGD}       // push retAddrLabel
+    @LCL
+    A=M
+    D=A
+    {PUSH_REGD}       // push LCL
+    @ARG
+    A=M
+    D=A
+    {PUSH_REGD}       // push ARG
+    @THIS
+    A=M
+    D=A
+    {PUSH_REGD}       // push THIS
+    @THAT
+    A=M
+    D=A
+    {PUSH_REGD}       // push THAT
+    @SP
+    D=M
+    @5
+    D=D-A
+    @{n_args}
+    D=D-A
+    @ARG
+    M=D               // ARG = SP - 5 - nArgs
+    @SP
+    D=M
+    @LCL
+    M=D               // LCL = SP
+    @{name}
+    0;JMP
+({ret_addr_label})"
+        )
     }
 
     pub fn write_return(&mut self) -> Result<()> {
@@ -294,61 +315,62 @@ M=M+1 // push {i}"
 
         writeln!(
             out,
-            "\
-@LCL
-D=M
-@R5
-M=D // endFrame = LCL
-@5
-D=D-A
-A=D
-D=M
-@R6
-M=D // retAddr = *(endFrame - 5)
-@SP
-AM=M-1
-D=M
-@ARG
-A=M
-M=D // *ARG = pop()
-D=A+1
-@SP
-M=D // SP = ARG + 1
-@R5
-D=M
-@1
-D=D-A
-A=D
-D=M
-@THAT
-M=D // THAT = *(endFrame - 1)
-@R5
-D=M
-@2
-D=D-A
-A=D
-D=M
-@THIS
-M=D // THIS = *(endFrame - 2)
-@R5
-D=M
-@3
-D=D-A
-A=D
-D=M
-@ARG
-M=D // ARG = *(endFrame - 3)
-@R5
-D=M
-@4
-D=D-A
-A=D
-D=M
-@LCL
-M=D // LCL = *(endFrame - 4)
-@R6
-A=M
-0;JMP // goto retAddr"
+            "    @LCL
+    D=M
+    @R13
+    M=D // endFrame = LCL
+    @R13
+    D=M
+    @5
+    D=D-A
+    A=D
+    D=M
+    @R14
+    M=D // retAddr = *(endFrame - 5)
+    @SP
+    AM=M-1
+    D=M
+    @ARG
+    A=M
+    M=D // *ARG = pop()
+    D=A+1
+    @SP
+    M=D // SP = ARG + 1
+    @R13
+    D=M
+    @1
+    D=D-A
+    A=D
+    D=M
+    @THAT
+    M=D // THAT = *(endFrame - 1)
+    @R13
+    D=M
+    @2
+    D=D-A
+    A=D
+    D=M
+    @THIS
+    M=D // THIS = *(endFrame - 2)
+    @R13
+    D=M
+    @3
+    D=D-A
+    A=D
+    D=M
+    @ARG
+    M=D // ARG = *(endFrame - 3)
+    @R13
+    D=M
+    @4
+    D=D-A
+    A=D
+    D=M
+    @LCL
+    M=D // LCL = *(endFrame - 4)
+    @R14
+    A=M
+    0;JMP // goto retAddr"
         )
     }
 }
@@ -363,9 +385,8 @@ fn get_segment_symbol(segment: &str) -> &'static str {
     }
 }
 
-const PUSH_REGD: &str = "\
-@SP
-A=M
-M=D
-@SP
-M=M+1";
+const PUSH_REGD: &str = "@SP
+    A=M
+    M=D
+    @SP
+    M=M+1";
--- vm-to-asm/src/main.rs
+++ vm-to-asm/src/main.rs
@@ -6,62 +6,66 @@ fn main() -> io::Result<()> {
     let args = env::args().skip(1).collect::<Vec<_>>();
 
     if args.is_empty() {
-        println!("help: vm-to-asm <input vm file>");
+        println!("help: vm-to-asm <input vm file..> [output asm file]");
         process::exit(1);
     }
 
-    let file = Path::new(&args[0]);
-    let dest = file.with_extension("asm");
-    let dest = dest.to_str().unwrap();
+    let (vm_files, asm_file): (Vec<_>, Vec<_>) = args.into_iter().partition(|f| f.ends_with(".vm"));
+
+    let dest = asm_file.first().expect("must provide an output asm file");
 
     let mut code_writer = CodeWriter::new();
     code_writer.set_file_name(dest);
+    // code_writer.write_init();
 
-    let input = fs::read_to_string(file)?;
+    for file in vm_files {
+        let input = fs::read_to_string(&file)?;
+        let mut parser = Parser::new(&input);
 
-    let mut parser = Parser::new(&input);
+        let file_name = Path::new(&file).file_name().unwrap().to_str().unwrap();
+        code_writer.set_module_name(file_name);
 
-    code_writer.set_module_name(file.file_name().unwrap().to_str().unwrap());
-    while parser.has_more_commands() {
-        let command_type = parser.command_type();
+        while parser.has_more_commands() {
+            let command_type = parser.command_type();
 
-        use CommandType::*;
-        match command_type {
-            Arithmetic => {
-                let command = parser.arg1();
-                code_writer.write_arithmetic(command)?;
+            use CommandType::*;
+            match command_type {
+                Arithmetic => {
+                    let command = parser.arg1();
+                    code_writer.write_arithmetic(command)?;
+                }
+                Push | Pop => {
+                    let segment = parser.arg1();
+                    let index = parser.arg2();
+                    code_writer.write_pushpop(command_type, segment, index)?;
+                }
+                Label => {
+                    let label = parser.arg1();
+                    code_writer.write_label(label)?;
+                }
+                If => {
+                    let label = parser.arg1();
+                    code_writer.write_if(label)?;
+                }
+                Goto => {
+                    let label = parser.arg1();
+                    code_writer.write_goto(label)?;
+                }
+                Function => {
+                    let function_name = parser.arg1();
+                    let n_vars = parser.arg2();
+                    code_writer.write_function(function_name, n_vars)?;
+                }
+                Call => {
+                    let function_name = parser.arg1();
+                    let n_args = parser.arg2();
+                    code_writer.write_call(function_name, n_args)?;
+                }
+                Return => code_writer.write_return()?,
             }
-            Push | Pop => {
-                let segment = parser.arg1();
-                let index = parser.arg2();
-                code_writer.write_pushpop(command_type, segment, index)?;
-            }
-            Label => {
-                let label = parser.arg1();
-                code_writer.write_label(label)?;
-            }
-            If => {
-                let label = parser.arg1();
-                code_writer.write_if(label)?;
-            }
-            Goto => {
-                let label = parser.arg1();
-                code_writer.write_goto(label)?;
-            }
-            Function => {
-                let function_name = parser.arg1();
-                let n_vars = parser.arg2();
-                code_writer.write_function(function_name, n_vars)?;
-            }
-            Call => {
-                let function_name = parser.arg1();
-                let n_args = parser.arg2();
-                code_writer.write_call(function_name, n_args)?;
-            }
-            Return => code_writer.write_return()?,
+
+            parser.advance();
         }
-
-        parser.advance();
     }
 
     Ok(())
